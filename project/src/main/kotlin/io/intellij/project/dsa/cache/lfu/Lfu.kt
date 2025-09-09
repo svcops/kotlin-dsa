6be866a58e7bb03c20ab19f7f4948ecfbd94925e
zhenjie zhu <tech@intellij.io>
2025-09-09 23:48:14 +0800

    ✏️ Update comments for LFU and LRU interfaces
    Enhanced the class-level comments in LFU and LRU interfaces to provide detailed explanations of their respective caching strategies. These updates improve code documentation and clarity, making the concepts easier to understand.

--- project/src/main/kotlin/io/intellij/project/dsa/cache/lfu/Lfu.kt
+++ project/src/main/kotlin/io/intellij/project/dsa/cache/lfu/Lfu.kt
@@ -1,7 +1,10 @@
 package io.intellij.project.dsa.cache.lfu
 
 /**
- * LFU
+ * LFU（Least Frequently Used，最不经常使用）是一种缓存淘汰策略：当缓存满了，需要删除一个键值时，优先淘汰“访问次数最少”的那个键
+ *
+ * - 每个键有一个“访问频次”计数，get/put 命中都会让频次 +1
+ * - 淘汰时选择全局频次最低的键；若有多个频次相同，一般再用“最近最少使用”（LRU）作为并列打破原则。
  *
  * @author tech@intellij.io
  */
--- project/src/main/kotlin/io/intellij/project/dsa/cache/lru/Lru.kt
+++ project/src/main/kotlin/io/intellij/project/dsa/cache/lru/Lru.kt
@@ -1,7 +1,11 @@
 package io.intellij.project.dsa.cache.lru
 
 /**
- * LRU(Least Recently Used) 最近最少使用缓存
+ * LRU（Least Recently Used，最近最少使用）是一种常见的缓存淘汰策略：当缓存容量满时，优先淘汰“最近最久没有被访问”的数据项。
+ *
+ * - 核心思想：越“久未使用”的数据越不重要，先被淘汰
+ * - 命中时机：每次 get 或 put 更新已有键，都将该键标记为“最近使用”
+ * - 淘汰规则：容量满时移除最久未被访问的那一项。
  *
  * @author tech@intellij.io
  */
